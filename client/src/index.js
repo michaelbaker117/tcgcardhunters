42ad4027fe3ef5d6c548f8653c7a51883b0de3cd
michaelbaker117 <70401610+michaelbaker117@users.noreply.github.com>
2026-04-01 02:20:50 -0400

    Add index.js to render React App to DOM

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Render the React App to the DOM
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
